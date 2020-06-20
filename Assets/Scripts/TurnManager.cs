055f071962e27ab3583c1a956567e7fdc452352a
GramNitschke <gnitschk@ucsc.edu>
2020-06-20 12:32:59 -0700

    Initial commit

---
+++ Assets/Scripts/TurnManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnManager : MonoBehaviour
+{
+    public Soil[] board;
+
+    public int turn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        turn = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //test
+    }
+}
