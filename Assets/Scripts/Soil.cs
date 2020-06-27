ada18a62a8f496436fd6b750a7f554783a0f24c5
GramNitschke <gnitschk@ucsc.edu>
2020-06-26 18:34:30 -0700

    inefficient check if you can do a move

--- Assets/Scripts/Soil.cs
+++ Assets/Scripts/Soil.cs
@@ -28,42 +28,61 @@ public class Soil : MonoBehaviour
         plant = null;
     }
 
-    public int Use(GameObject toBeUsed)
+    public int Use(GameObject toBeUsed, bool check)
     {
         if (plantObject == null && toBeUsed.GetComponent<Flora>() != null)
         {
-            Debug.Log("Planted");
-            //name = item;
-            growth = 0;
-            plantObject = Instantiate(toBeUsed);
-            plant = plantObject.GetComponent<Flora>();
-            currentStageObject = Instantiate(plant.stages[plant.currStage].model, transform);
-            toBeUsed.GetComponent<Item>().energyRequired = plant.currStage;
+            if (!check)
+            {
+                Debug.Log("Planted");
+                growth = 0;
+                plantObject = Instantiate(toBeUsed);
+                plant = plantObject.GetComponent<Flora>();
+                currentStageObject = Instantiate(plant.stages[plant.currStage].model, transform);
+            }
+            toBeUsed.GetComponent<Item>().energyRequired = toBeUsed.GetComponent<Flora>().currStage + 1;
         }
         else if(toBeUsed.GetComponent<Tool>() != null)
         {
-            if (toBeUsed.name == "Shovel" && plantObject != null)
+            if (toBeUsed.name == "Gloves" && plantObject != null)
             {
-                Debug.Log("Ripped up");
-                toBeUsed.GetComponent<Item>().energyRequired = plant.currStage;
-                Destroy(plantObject);
-                Destroy(currentStageObject);
-                plantObject = null;
-                plant = null;
+                toBeUsed.GetComponent<Item>().energyRequired = plant.currStage + 1;
+                if (!check)
+                {
+                    Debug.Log("Ripped up");
+                    Destroy(plantObject);
+                    Destroy(currentStageObject);
+                    plantObject = null;
+                    plant = null;
+                }
             }
             else
             {
-                Debug.Log(toBeUsed.name + " cannot be used here.");
-                return 0;
+                //Debug.Log(toBeUsed.name + " cannot be used here.");
+                return 101;
             }
         }
         else
         {
-            Debug.Log("Not a valid item to use");
-            return 0;
+            //Debug.Log("Not a valid item to use");
+            return 101;
         }
 
         return toBeUsed.GetComponent<Item>().energyRequired;
+    }
+
+    //TODO
+    public int getEnergyRequired(GameObject toBeUsed)
+    {
+        Item item = toBeUsed.GetComponent<Item>();
+        if (item.energyRequired >= 0)
+        {
+            return item.energyRequired;
+        }
+        else
+        {
+            return 1;
+        }
     }
 
     public void Grow()
--- Assets/Scripts/TurnManager.cs
+++ Assets/Scripts/TurnManager.cs
@@ -18,6 +18,8 @@ public class TurnManager : MonoBehaviour
     private int energy;
     private Text energyUI;
 
+    private Text mouseInfo;
+
     public GameObject currentItem;
     // Start is called before the first frame update
     void Start()
@@ -28,6 +30,7 @@ public class TurnManager : MonoBehaviour
         turnUI = transform.Find("Canvas").Find("Turn").GetComponent<Text>();
         turnUI.text = "Turn: " + turn;
         energyUI = transform.Find("Canvas").Find("Energy").GetComponent<Text>();
+        mouseInfo = transform.Find("Canvas").Find("MouseInfo").GetComponent<Text>();
         energyUI.text = "Energy: " + energy;
         board = new Soil[boardWidth, boardLength];
         GenerateBoard();
@@ -36,22 +39,45 @@ public class TurnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-            if (Physics.Raycast(ray, out hit, float.PositiveInfinity, 1 << 9) && hit.transform.GetComponent<Soil>() != null)
+        if (Physics.Raycast(ray, out hit, float.PositiveInfinity, 1 << 9) && hit.transform.GetComponent<Soil>() != null)
+        {
+            if (currentItem != null)
             {
-                if (currentItem != null && energy >= currentItem.GetComponent<Item>().energyRequired)
+                int e = hit.transform.GetComponent<Soil>().Use(currentItem, true);
+                if (Input.GetMouseButtonUp(0) && energy >= e)
                 {
-                    energy -= hit.transform.GetComponent<Soil>().Use(currentItem);
+                    energy -= hit.transform.GetComponent<Soil>().Use(currentItem, false);
                     energyUI.text = "Energy: " + energy;
                 }
                 else
-                    Debug.Log("Please select an item");
+                {
+                    string mI = "X";
+                    if(e != 101)
+                    {
+                        mI = "E: -" + e;
+                    }
+
+                    if(energy < e || e == 101)
+                    {
+                        mouseInfo.color = Color.red;
+                    }
+                    else
+                    {
+                        mouseInfo.color = Color.white;
+                    }
+                    mouseInfo.text = mI;
+                }
             }
         }
+        else
+        {
+            mouseInfo.text = "";
+        }
+        
     }
 
     private void GenerateBoard()
